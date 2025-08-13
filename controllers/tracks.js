32ef611f556eadea0bfc48c838aa8d8e8f5cf4e3
pericarney00 <pericarney@gmail.com>
2025-08-13 14:59:30 -0500

    Created CRUD and tested CRUD routes

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -14,15 +14,71 @@ router.post("/", async (req, res) => {
 })
 
 //INDEX-Get-/tracks res200
-
+router.get("/", async (req, res) => {
+  try {
+    const foundTracks = await Track.find()
+    res.status(200).json({foundTracks})
+  } catch (error) {
+    res.status(500).json({error:error.message})
+  }
+})
 
 //SHOW-GET-/tracks/:id res200
+router.get("/:trackId", async (req, res) => {
+  try {
+    const findTrack = await Track.findById(req.params.trackId)
+    if (!findTrack) {
+      res.status(400)
+      throw new Error("Track not found")
+    }
 
+    res.status(200).json(findTrack)
+  } catch (error) {
+    if (res.statusCode === 400) {
+      res.json({error: error.message}) 
+    } else {
+      res.status(500).json({error: error.message})
+    }
+  }
+})
 
 //UPDATE-PUT-/tracks/:id res200
+router.put("/:trackId", async (req, res) => {
+  try {
+    const updateTrack = await Track.findByIdAndUpdate(req.params.trackId, req.body, { new: true })
+    if (!updateTrack) {
+      res.status(400)
+      throw new Error("Track not found")
+    }
 
+    res.status(200).json(updateTrack)
+  } catch (error) {
+    if (res.statusCode === 400) {
+      res.json({ error: error.message });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  }
+})
 
 //DELETE-DELETE-/tracks/:id res200
+router.delete("/:trackId", async (req, res) => {
+  try {
+    const deletedTrack = await Track.findByIdAndDelete(req.params.trackId);
 
+    if (!deletedTrack) {
+      res.status(404);
+      throw new Error("Pet not found");
+    }
+
+    res.status(200).json(deletedTrack);
+  } catch (error) {
+    if (res.statusCode === 404) {
+      res.json({ error: error.message });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  }
+});
 
 module.exports = router
