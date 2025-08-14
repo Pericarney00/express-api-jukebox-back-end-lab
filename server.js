d1c9443fe998b3deccb534077b0a7a0a0b8e8c73
pericarney00 <pericarney@gmail.com>
2025-08-14 16:53:57 -0500

    installed and imported cors

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const trackRouter = require("./controllers/tracks")
+const cors = require("cors");
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -14,6 +16,7 @@ mongoose.connection.on("connected", () => {
 
 app.use(express.json());
 app.use(logger("dev"));
+app.use(cors());
 app.use("/tracks", trackRouter)
 
 // Routes go here
